0d78388ab546761da377177a37e48208d9a50c60
Nathan Downs <n8downs@gmail.com>
2015-08-18 11:24:07 -0700

    Add test coverage for insolvency detection.

--- main.go
+++ main.go
@@ -5,70 +5,76 @@ import (
 	"math"
 	"time"
 
-	t "github.com/ndowns/even_challenge/Types"
+	"github.com/ndowns/even_challenge/Types"
 )
 
 func main() {
-	startDay, _ := time.Parse(t.DateFormat, "2015.08.01")
-	endDay, _ := time.Parse(t.DateFormat, "2015.08.31")
+	startDay, _ := time.Parse(Types.DateFormat, "2015.08.01")
+	endDay, _ := time.Parse(Types.DateFormat, "2015.08.31")
 
-	incomes := []t.Income{}
+	incomes := []Types.Income{}
 	incomes = append(incomes,
-		t.Income{
+		Types.Income{
 			Amount:   500.,
 			Name:     "Philz",
-			Schedule: t.Schedule{Period: t.BiMonthly},
+			Schedule: Types.Schedule{Period: Types.BiMonthly},
 		},
-		t.Income{
+		Types.Income{
 			Amount:   175.,
 			Name:     "Mission Cliffs",
-			Schedule: t.Schedule{Period: t.BiWeekly, Weekday: time.Thursday},
+			Schedule: Types.Schedule{Period: Types.BiWeekly, Weekday: time.Thursday},
 		})
 	fmt.Println("Incomes:  ", incomes)
 
-	expenses := []t.Expense{}
+	expenses := []Types.Expense{}
 	expenses = append(expenses,
-		t.Expense{
+		Types.Expense{
 			Amount:   42.34,
 			Name:     "Utilities",
-			Schedule: t.Schedule{Period: t.Monthly, Date: 25},
+			Schedule: Types.Schedule{Period: Types.Monthly, Date: 25},
 		},
-		t.Expense{
+		Types.Expense{
 			Amount:   400.,
 			Name:     "Rent",
-			Schedule: t.Schedule{Period: t.Monthly, Date: 28},
+			Schedule: Types.Schedule{Period: Types.Monthly, Date: 28},
 		},
-		t.Expense{
+		Types.Expense{
 			Amount:   40.,
 			Name:     "Crossfit",
-			Schedule: t.Schedule{Period: t.Weekly, Weekday: time.Tuesday},
+			Schedule: Types.Schedule{Period: Types.Weekly, Weekday: time.Tuesday},
 		},
 	)
 	fmt.Println("Expenses: ", expenses)
 
-	_ = simulate(startDay, endDay, incomes, expenses)
+	plan := Plan(startDay, endDay, incomes, expenses)
+
+	_ = Simulate(startDay, endDay, plan, true)
 }
 
-func simulate(startDay time.Time, endDay time.Time, incomes []t.Income, expenses []t.Expense) map[t.Account]float64 {
-	accounts := map[t.Account]float64{t.External: 0., t.Checking: 0., t.Savings: 0.}
-
+// Plan ...
+func Plan(
+	startDay time.Time,
+	endDay time.Time,
+	incomes []Types.Income,
+	expenses []Types.Expense,
+) (ledger map[time.Time][]Types.Transaction) {
+	ledger = map[time.Time][]Types.Transaction{}
 	totalIncome := 0.
 	totalExpenses := 0.
 
 	incomeTotals := map[time.Time]float64{}
 	savingsPlan := map[time.Time]float64{}
 	expenseTotals := map[time.Time]float64{}
-	ledger := map[time.Time][]t.Transaction{}
 
 	for _, income := range incomes {
 		occurrances := income.Schedule.FindOccurrances(startDay, endDay)
 		for _, date := range occurrances {
-			ledger[date] = append(ledger[date], t.Transaction{
+			ledger[date] = append(ledger[date], Types.Transaction{
 				Date:  date,
 				Delta: income.Amount,
 				Memo:  fmt.Sprintf("Income: %s", income.Name),
-				From:  t.External,
-				To:    t.Checking,
+				From:  Types.External,
+				To:    Types.Checking,
 			})
 			totalIncome += income.Amount
 			incomeTotals[date] += income.Amount
@@ -85,10 +91,10 @@ func simulate(startDay time.Time, endDay time.Time, incomes []t.Income, expenses
 	}
 
 	idealDiscretionary := (totalIncome - totalExpenses) / (endDay.Sub(startDay).Hours() / 24)
-	fmt.Printf("Total: $%.2f in, $%.2f out, $%.2f ideally per day\n\n", totalIncome, totalExpenses, idealDiscretionary)
+	//fmt.Printf("Total: $%.2f in, $%.2f out, $%.2f ideally per day\n\n", totalIncome, totalExpenses, idealDiscretionary)
 	if totalExpenses > totalIncome {
-		fmt.Printf("Insolvent :(")
-		return accounts
+		//fmt.Printf("Insolvent :(")
+		return map[time.Time][]Types.Transaction{}
 	}
 
 	discretionaryAmount := 0.
@@ -130,10 +136,10 @@ func simulate(startDay time.Time, endDay time.Time, incomes []t.Income, expenses
 				if simulatedSpendingDate.Equal(nextIncomeDate) {
 					break
 				}
-				ledger[simulatedSpendingDate] = append(ledger[simulatedSpendingDate], t.Transaction{
-					From:  t.Checking,
-					To:    t.External,
-					Memo:  "Simulated Spending",
+				ledger[simulatedSpendingDate] = append(ledger[simulatedSpendingDate], Types.Transaction{
+					From:  Types.Checking,
+					To:    Types.External,
+					Memo:  "  -Simulated Spending-",
 					Date:  simulatedSpendingDate,
 					Delta: -1 * actualDiscretionary,
 				})
@@ -143,24 +149,24 @@ func simulate(startDay time.Time, endDay time.Time, incomes []t.Income, expenses
 		currentDate = currentDate.AddDate(0, 0, 1)
 	}
 
-	fmt.Printf("Planned average discretionary spending levels: $%.2f (%.2f%%)\n\n", discretionaryAmount/discretionaryDays, discretionaryAmount/discretionaryDays/idealDiscretionary)
+	//fmt.Printf("Planned average discretionary spending levels: $%.2f (%.2f%%)\n\n", discretionaryAmount/discretionaryDays, discretionaryAmount/discretionaryDays/idealDiscretionary)
 
 	for date, amount := range savingsPlan {
 		if amount < 0 {
-			ledger[date] = append(ledger[date], t.Transaction{
+			ledger[date] = append(ledger[date], Types.Transaction{
 				Date:  date,
 				Delta: amount,
 				Memo:  "Transfer to Savings",
-				From:  t.Checking,
-				To:    t.Savings,
+				From:  Types.Checking,
+				To:    Types.Savings,
 			})
 		} else {
-			ledger[date] = append(ledger[date], t.Transaction{
+			ledger[date] = append(ledger[date], Types.Transaction{
 				Date:  date,
 				Delta: amount,
 				Memo:  "Transfer from Savings",
-				From:  t.Savings,
-				To:    t.Checking,
+				From:  Types.Savings,
+				To:    Types.Checking,
 			})
 		}
 	}
@@ -168,17 +174,23 @@ func simulate(startDay time.Time, endDay time.Time, incomes []t.Income, expenses
 	for _, expense := range expenses {
 		occurrances := expense.Schedule.FindOccurrances(startDay, endDay)
 		for _, date := range occurrances {
-			ledger[date] = append(ledger[date], t.Transaction{
+			ledger[date] = append(ledger[date], Types.Transaction{
 				Date:  date,
 				Delta: -1 * expense.Amount,
 				Memo:  fmt.Sprintf("Expense: %s", expense.Name),
-				From:  t.Savings,
-				To:    t.External,
+				From:  Types.Savings,
+				To:    Types.External,
 			})
 		}
 	}
+	return
+}
 
-	currentDate = startDay
+// Simulate ...
+func Simulate(startDay time.Time, endDay time.Time, ledger map[time.Time][]Types.Transaction, shouldPrintOutput bool) (accounts map[Types.Account]float64) {
+	accounts = map[Types.Account]float64{Types.External: 0., Types.Checking: 0., Types.Savings: 0.}
+
+	currentDate := startDay
 	inARow := 0
 	for {
 		if currentDate.After(endDay) {
@@ -188,7 +200,7 @@ func simulate(startDay time.Time, endDay time.Time, incomes []t.Income, expenses
 		transactions := ledger[currentDate]
 		if len(transactions) == 0 {
 			inARow++
-			if inARow <= 1 {
+			if shouldPrintOutput && inARow <= 1 {
 				fmt.Println("    ...    |")
 			}
 		} else {
@@ -198,7 +210,9 @@ func simulate(startDay time.Time, endDay time.Time, incomes []t.Income, expenses
 		for _, transaction := range transactions {
 			accounts[transaction.From] -= math.Abs(transaction.Delta)
 			accounts[transaction.To] += math.Abs(transaction.Delta)
-			fmt.Printf("%s | %7.2f | %7.2f\n", transaction.ToString(), accounts[t.Checking], accounts[t.Savings])
+			if shouldPrintOutput {
+				fmt.Printf("%s | %7.2f | %7.2f\n", transaction.ToString(), accounts[Types.Checking], accounts[Types.Savings])
+			}
 		}
 		currentDate = currentDate.AddDate(0, 0, 1)
 	}
--- main_test.go
+++ main_test.go
@@ -2,10 +2,85 @@ package main
 
 import (
 	"testing"
+	"time"
 
+	"github.com/ndowns/even_challenge/Types"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFoo(t *testing.T) {
-	assert.Equal(t, "foo", "foo")
+func TestBasicHappyPath(t *testing.T) {
+	startDay, _ := time.Parse(Types.DateFormat, "2015.08.01")
+	endDay, _ := time.Parse(Types.DateFormat, "2015.08.31")
+
+	incomes := []Types.Income{
+		Types.Income{
+			Amount:   500.,
+			Name:     "Philz",
+			Schedule: Types.Schedule{Period: Types.BiMonthly},
+		},
+		Types.Income{
+			Amount:   175.,
+			Name:     "Mission Cliffs",
+			Schedule: Types.Schedule{Period: Types.BiWeekly, Weekday: time.Thursday},
+		},
+	}
+
+	expenses := []Types.Expense{
+		Types.Expense{
+			Amount:   42.34,
+			Name:     "Utilities",
+			Schedule: Types.Schedule{Period: Types.Monthly, Date: 25},
+		},
+		Types.Expense{
+			Amount:   400.,
+			Name:     "Rent",
+			Schedule: Types.Schedule{Period: Types.Monthly, Date: 28},
+		},
+		Types.Expense{
+			Amount:   40.,
+			Name:     "Crossfit",
+			Schedule: Types.Schedule{Period: Types.Weekly, Weekday: time.Tuesday},
+		},
+	}
+
+	plan := Plan(startDay, endDay, incomes, expenses)
+	accounts := Simulate(startDay, endDay, plan, false)
+
+	assert.InDelta(t, accounts[Types.External], 0, 0.001)
+	assert.InDelta(t, accounts[Types.Checking], 0, 0.001)
+	assert.InDelta(t, accounts[Types.Savings], 0, 0.001)
+}
+
+func TestInsolvent(t *testing.T) {
+	startDay, _ := time.Parse(Types.DateFormat, "2015.08.01")
+	endDay, _ := time.Parse(Types.DateFormat, "2015.08.31")
+
+	incomes := []Types.Income{
+		Types.Income{
+			Amount:   500.,
+			Name:     "Philz",
+			Schedule: Types.Schedule{Period: Types.BiMonthly},
+		},
+	}
+
+	expenses := []Types.Expense{
+		Types.Expense{
+			Amount:   42.34,
+			Name:     "Utilities",
+			Schedule: Types.Schedule{Period: Types.Monthly, Date: 25},
+		},
+		Types.Expense{
+			Amount:   1200.,
+			Name:     "Rent",
+			Schedule: Types.Schedule{Period: Types.Monthly, Date: 28},
+		},
+		Types.Expense{
+			Amount:   40.,
+			Name:     "Crossfit",
+			Schedule: Types.Schedule{Period: Types.Weekly, Weekday: time.Tuesday},
+		},
+	}
+
+	plan := Plan(startDay, endDay, incomes, expenses)
+	assert.Equal(t, 0, len(plan))
 }
